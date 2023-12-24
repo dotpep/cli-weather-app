9f9c2b05eec1bf41297eae646ae4cc1d39156f05
dotpep <pepelnine@gmail.com>
2023-12-24 20:53:29 +0600

    refactor: Modularize weather API service and improve error handling
    - Reorganized the `weather_api_service.py` module for better modularity.
    - Improved error handling throughout the module for better reliability.
    - Updated weather type string representations for clarity.
    - Encapsulated the API request and response handling in private functions.
    - Added type hints for better code readability.

--- weather_api_service.py
+++ weather_api_service.py
@@ -1,23 +1,23 @@
 from datetime import datetime
 import json
-# from json.decoder import JSONDecodeError
-# import ssl
+from json.decoder import JSONDecodeError
+import ssl
 import urllib.request
-# from urllib.error import URLError
+from urllib.error import URLError
 
 from enum import Enum
 from typing import Literal, NamedTuple, TypeAlias
 
 import config
 from coordinates import Coordinates
-# from exceptions import ApiServiceError
+from exceptions import ApiServiceError
 
 Celsius: TypeAlias = int
 
 
-class WeatherType(Enum):
+class WeatherType(str, Enum):
     THUNDERSTORM = "Гроза"
-    DRIZZLE = "Изморозь"
+    DRIZZLE = "Изморось"
     RAIN = "Дождь"
     SNOW = "Снег"
     CLEAR = "Ясно"
@@ -35,30 +35,85 @@ class Weather(NamedTuple):
 
 def get_weather(coordinates: Coordinates) -> Weather:
     """Requests weather in OpenWeatherMap API and returns it"""
-    lat, lon = coordinates
+    openweather_response = _get_openweather_response(
+        longitude=coordinates.longitude, latitude=coordinates.latitude
+    )
+    weather = _parse_openweather_response(openweather_response)
+    return weather
+
+
+def _get_openweather_response(latitude: float, longitude: float) -> str:
+    ssl._create_default_https_context = ssl._create_unverified_context
+    # TODO: Use GPS or Input, add functionality of input name of City if GPS locator coordination doesn't work
+    # if user set config.USE_GPS == True; then return url with "city api query"
+
+    # if config.USE_GPS:
+    #     url = config.OPENWEATHER_URL_TEMPLATE.format(
+    #         latitude=latitude, longitude=longitude
+    #     )
+    # else:
+    #     url = config.OPENWEATHER_CITY_URL_TEMPLATE.format(
+    #         city="Almaty"
+    #     )
 
     url = config.OPENWEATHER_URL_TEMPLATE.format(
-        latitude=lat, longitude=lon
+        latitude=latitude, longitude=longitude
     )
+    try:
+        return urllib.request.urlopen(url).read()
+    except URLError:
+        raise ApiServiceError
 
-    openweather_response = urllib.request.urlopen(url).read()
-    openweather_dict = json.loads(openweather_response)
 
-    temperature = openweather_dict["main"]["temp"]
-    weather_type = openweather_dict["weather"][0]["id"]
-    # sunrise = openweather_dict["sys"]["sunrise"]
-    # sunset = openweather_dict["sys"]["sunset"]
-    sunrise = datetime.fromtimestamp(openweather_dict["sys"]["sunrise"])
-    sunset = datetime.fromtimestamp(openweather_dict["sys"]["sunset"])
-    city = openweather_dict["name"]
-
+def _parse_openweather_response(openweather_response: str) -> Weather:
+    try:
+        openweather_dict = json.loads(openweather_response)
+    except JSONDecodeError:
+        raise ApiServiceError
     return Weather(
-        temperature=temperature,
-        weather_type=weather_type,
-        sunrise=sunrise,
-        sunset=sunset,
-        city=city,
+        temperature=_parse_temperature(openweather_dict),
+        weather_type=_parse_weather_type(openweather_dict),
+        sunrise=_parse_sun_time(openweather_dict, "sunrise"),
+        sunset=_parse_sun_time(openweather_dict, "sunset"),
+        city=_parse_city(openweather_dict)
     )
+
+
+def _parse_temperature(openweather_dict: dict) -> Celsius:
+    return round(openweather_dict["main"]["temp"])
+
+
+def _parse_weather_type(openweather_dict: dict) -> WeatherType:
+    try:
+        weather_type_id = str(openweather_dict["weather"][0]["id"])
+    except (IndexError, KeyError):
+        raise ApiServiceError
+    weather_types = {
+        "1": WeatherType.THUNDERSTORM,
+        "3": WeatherType.DRIZZLE,
+        "5": WeatherType.RAIN,
+        "6": WeatherType.SNOW,
+        "7": WeatherType.FOG,
+        "800": WeatherType.CLEAR,
+        "80": WeatherType.CLOUDS
+    }
+    for _id, _weather_type in weather_types.items():
+        if weather_type_id.startswith(_id):
+            return _weather_type
+    raise ApiServiceError
+
+
+def _parse_sun_time(
+        openweather_dict: dict,
+        time: Literal["sunrise"] | Literal["sunset"]) -> datetime:
+    return datetime.fromtimestamp(openweather_dict["sys"][time])
+
+
+def _parse_city(openweather_dict: dict) -> str:
+    try:
+        return openweather_dict["name"]
+    except KeyError:
+        raise ApiServiceError
 
 
 if __name__ == "__main__":
